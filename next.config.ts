e566667a669061d0350880f39871beffb8b0cd8a
awselliottai <eamarquez17@gmail.com>
2026-09-17 08:49:04 -0500

    Updated next.config to allow testing access (ngrok).

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['makhi-coldturkey-brady.ngrok-free.dev'],
+  devIndicators: false,
 };
 
 export default nextConfig;
