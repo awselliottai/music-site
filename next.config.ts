db4eea8799be69f1de6709759249f5b031cb26ce
awselliottai <eamarquez17@gmail.com>
2026-09-17 08:46:38 -0500

    Updated next config button access issue.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['makhi-coldturkey-brady.ngrok-free.dev'],
+  devIndicators: false,
 };
 
 export default nextConfig;
